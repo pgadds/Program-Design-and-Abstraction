1dbb547f57704da4d29e6a4c1f6320ca390feb46
Pranav Gaddameedi <60829558+pgadds@users.noreply.github.com>
2021-02-08 19:18:48 -0500

    Add files via upload

---
+++ Problem Set 1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  7 19:21:02 2019
+
+@author: navboi
+"""
+
+#data = str(input("Please enter a lowercase string: "))
+#vowels = "aeiou"
+#for v in vowels:
+#    print('the number of vowels ', v, data.lower().count(v))
+
+
+#count= 0 
+#s= input("enter a string of letters: ")
+#for i in range(len(s)):
+#    if s[i:i+3]=="lol":
+#        count+=1
+#print("the number of times 'lol' was in the string was: ", count)
+
+#s = str(input("enter a string of lowercase letters: "))
+#lenstring = s[0]
+#slen = 1
+#for i in range(len(s)):
+#    for j in range(i,len(s)-1):
+#            if s[j+1] >= s[j]:
+#                    if (j+1)-i+1 > slen:
+#                        lenstring = s[i:(j+1)+1]
+#                        slen = (j+1)-i+1
+#            else:
+#                        break
+#
+#print("Longest substring in alphabetical order is: " + lenstring)
